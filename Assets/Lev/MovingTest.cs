d87a91cfdabf26543439b6801cc8e8e8cbb0687f
Leondopulus <arcinside@yahoo.com>
2022-09-02 11:15:45 +0300

    adding blind filter

--- Assets/Lev/MovingTest.cs
+++ Assets/Lev/MovingTest.cs
@@ -6,23 +6,34 @@ using UnityEngine;
 public class MovingTest : MonoBehaviour
 {
     public Transform playerTransform;
+    public Transform filterTransform;
+    public  float distanceToFilter;
    
     void Start()
     {
         playerTransform = GameObject.FindWithTag("Player").transform;
+        filterTransform = GameObject.FindWithTag("BlindFilter").transform;
     }
 
     void Update()
     {
-        
-        float distance = Vector3.Distance(playerTransform.position, transform.position);
-        Debug.Log(distance);
-        if (distance <= 1f)
+        float distanceToPlayer = Vector3.Distance(playerTransform.position, transform.position);
+        // Debug.Log(distanceToPlayer);
+        if (distanceToPlayer <= 1f)
         {
             transform.Rotate(0, 60, 0);
         }
         
+        distanceToFilter = Vector3.Distance(filterTransform.position, transform.position);
+        if (distanceToFilter <= 0.1f)
+        {
+            addBlindFilter(transform);
+        }
     }
 
+    void addBlindFilter(Transform leverTransform)
+    { 
+        Debug.Log("addBlindFilter DONE ");
+    }
 
 }
